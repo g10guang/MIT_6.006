de6b26182482339287a8f332f88cc93d72ec1215
g10guang <g10guang@gmail.com>
2018-01-20 17:12:54 +0800

    添加牛顿法求解任意精度的根号2

---
+++ course11/square2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+# coding=utf-8
+# author: Xiguang Liu<g10guang@foxmail.com>
+# 2018-01-20 16:47
+# 使用牛顿法计算 根号2 的值，要求精度为 100M 个小数
+# 牛顿法的精度是成 2 的倍数指数增长的 1 --> 2 --> 4
+# 构造方程 f(x) = x^2 - 2 求解 x 满足 f(x) = 0，该 x = 根号2
+# 迭代式：x(i+1) = x(i)/2+1/x(i)
+
+import math
+
+
+def square2(d):
+    """
+    计算 d-digit of precision 根号 2
+    :param d: 需要多少位精度
+    :return:
+    """
+    # 从 1 位精度小数开始
+    result = 1.4
+    cal_times = int(log2(d))
+    for _ in range(cal_times):
+        result = result / 2 + 1 / result
+    return result
+
+
+def log2(d):
+    """
+    计算 log2 d base is 2
+    :param d:
+    :return:
+    """
+    return math.log2(d)
+
+
+if __name__ == '__main__':
+    result = square2(10*3)
+    print('{:.1000f}'.format(result))
